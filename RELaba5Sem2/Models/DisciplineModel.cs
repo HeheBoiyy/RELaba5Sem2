4dd81a5a36f19d0c787c0c83910cf815a1e01b30
HeheBoiyy <school152qq@mail.ru>
2024-04-09 03:08:25 +0700

    Добавьте файлы проекта.

---
+++ RELaba5Sem2/Models/DisciplineModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RELaba5Sem2.Models
+{
+    public class Discipline : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _teacherName;
+        private DateTime _examDate;
+        private string _difficulty;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string TeacherName
+        {
+            get { return _teacherName; }
+            set
+            {
+                _teacherName = value;
+                OnPropertyChanged("TeacherName");
+            }
+        }
+
+        public DateTime ExamDate
+        {
+            get { return _examDate; }
+            set
+            {
+                _examDate = value;
+                OnPropertyChanged("ExamDate");
+            }
+        }
+
+        public string Difficulty
+        {
+            get { return _difficulty; }
+            set
+            {
+                _difficulty = value;
+                OnPropertyChanged("Difficulty");
+            }
+        }
+
+        public DateTime PrepStartDate
+        {
+            get
+            {
+                switch (_difficulty)
+                {
+                    case "Лёгкий":
+                        return _examDate;
+                    case "Средний":
+                        return _examDate.AddDays(-14);
+                    case "Сложный":
+                        return _examDate.AddMonths(-1);
+                    default:
+                        return _examDate;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RELaba5Sem2/ViewModel/DisciplineViewModel.cs
@@ -0,0 +1,52 @@
+﻿using RELaba5Sem2.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RELaba5Sem2.src;
+using System.Windows;
+namespace RELaba5Sem2.ViewModel
+{
+    public class DisciplineViewModel
+    {
+        MainWindow mv = (MainWindow)Application.Current.MainWindow;
+        public ObservableCollection<Discipline> Disciplines { get; set; }
+
+        public Discipline SelectedItem { get; set; }
+
+        public DisciplineViewModel()
+        {
+            Disciplines = new ObservableCollection<Discipline>();
+            mv.Difficulty.Items.Add("Лёгкий");
+            mv.Difficulty.Items.Add("Средний");
+            mv.Difficulty.Items.Add("Сложный");
+        }
+        public RelayCommand AddDiscipline => new RelayCommand(execute => _AddDiscipline(mv.NameBut.Text, mv.FIOBut.Text, mv.Difficulty.SelectedItem.ToString(), mv.TimeEx.SelectedDate.GetValueOrDefault()));
+        public RelayCommand RemoveDiscipline => new RelayCommand(execute => _RemoveDiscipline(SelectedItem));
+        public void _AddDiscipline(string name, string tname, string diff, DateTime Date)
+        {
+            if (name == "" || tname =="" || diff ==null || Date == DateTime.MinValue)
+            {
+                MessageBox.Show("Какое то из полей не заполнено");
+            }
+            else
+            {
+                Discipline temp = new Discipline()
+                {
+                    Name = name,
+                    TeacherName = tname,
+                    Difficulty = diff,
+                    ExamDate = Date
+                };
+                Disciplines.Add(temp);
+            }
+        }
+
+        public void _RemoveDiscipline(Discipline discipline)
+        {
+            Disciplines.Remove(discipline);
+        }
+    }
+}
